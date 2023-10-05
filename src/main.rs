047815cdbc3cbf72a02c07339bbbdfd7d83fd589
root309 <choco00101x@gmail.com>
2023-10-05 23:07:58 +0900

    Init

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,27 @@
+use std::sync::{Arc, Mutex};
+use std::thread;
+
 fn main() {
-    println!("Hello, world!");
+    // 共有メモリに格納するデータ
+    let data = Arc::new(Mutex::new(0));
+
+    // スレッドを作成して共有メモリを操作
+    let mut handles = vec![];
+    for _ in 0..4 {
+        let data = Arc::clone(&data);
+        let handle = thread::spawn(move || {
+            let mut data = data.lock().unwrap();
+            *data += 1;
+        });
+        handles.push(handle);
+    }
+
+    // スレッドの終了を待つ
+    for handle in handles {
+        handle.join().unwrap();
+    }
+
+    // 共有メモリ内のデータを表示
+    let result = data.lock().unwrap();
+    println!("Final result: {}", *result);
 }
